bb169f2c03c83ba1bc5f4bcaec29db83d1f7dbd7
Papp Imre <imi950127@gmail.com>
2019-12-13 17:33:45 +0100

    Integrated doctrine, book entity. Listing datas in index template.

--- src/AppBundle/Controller/BookController.php
+++ src/AppBundle/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Book;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,9 +17,30 @@ class BookController extends Controller
      * @Route("/")
      * @Method({"GET"})
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('books/index.html.twig', array('name' => 'Test book'));
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        return $this->render('books/index.html.twig', array('books' => $books));
     }
+
+//    /**
+//     * @Route("/book/save")
+//     */
+//    public function save()
+//    {
+//        $entityManager = $this->getDoctrine()->getManager();
+//
+//        $book = new Book();
+//        $book->setIsbn("adfds3455234234test");
+//        $book->setAuthor("Testv2 Testv2");
+//        $book->setReleaseYear(2007);
+//        $book->setTitle("Testv2");
+//
+//        $entityManager->persist($book);
+//
+//        $entityManager->flush();
+//
+//        return new Response('Saved a book with the id: ' . $book->getId());
+//    }
 }
 
---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Book")
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $release_year;
+
+    /**
+     * @return mixed
+     */
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * @param mixed $isbn
+     */
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReleaseYear()
+    {
+        return $this->release_year;
+    }
+
+    /**
+     * @param mixed $release_year
+     */
+    public function setReleaseYear($release_year)
+    {
+        $this->release_year = $release_year;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
+
